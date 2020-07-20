26423b6a0ffdbb84ada3f91ccb8fe5237a83574a
Okello Nelson <cn.okello@live.com>
2020-07-20 20:07:17 +0200

    Deleted the now redudant stubs and comments

--- test/TestStarNotary.js
+++ test/TestStarNotary.js
@@ -7,6 +7,8 @@ contract('StarNotary', (accs) => {
     accounts = accs;
     owner = accounts[0];
 
+    // Implement Task 2 Add supporting unit tests
+
     it('can add the star name and star symbol properly', () => {
         let notary;
         let name = 'Awesome Star';
@@ -146,30 +148,4 @@ it('lets user2 buy a star and decreases its balance in ether', async() => {
     const balanceAfterUser2BuysStar = await web3.eth.getBalance(user2);
     let value = Number(balanceOfUser2BeforeTransaction) - Number(balanceAfterUser2BuysStar);
     assert.equal(value, starPrice);
-});
-
-// Implement Task 2 Add supporting unit tests
-
-it('can add the star name and star symbol properly', () => {
-    // 1. create a Star with different tokenId
-    //2. Call the name and symbol properties in your Smart Contract and compare with the name and symbol provided
-
-});
-
-it('lets 2 users exchange stars', async() => {
-    // 1. create 2 Stars with different tokenId
-    // 2. Call the exchangeStars functions implemented in the Smart Contract
-    // 3. Verify that the owners changed
-});
-
-it('lets a user transfer a star', async() => {
-    // 1. create a Star with different tokenId
-    // 2. use the transferStar function implemented in the Smart Contract
-    // 3. Verify the star owner changed.
-});
-
-it('lookUptokenIdToStarInfo test', async() => {
-    // 1. create a Star with different tokenId
-    // 2. Call your method lookUptokenIdToStarInfo
-    // 3. Verify if you Star name is the same
 });
